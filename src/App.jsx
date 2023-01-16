05ca03251a30eea97d6728f09bb4c053e931b05d
DarioCastel <lecastelnovo@gmail.com>
2023-01-16 12:38:22 -0300

    sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,56 @@
 import "./App.css";
+import image1 from "./img/ilidan.png"
 
 function App() {
   return (
     <div className="App">
-      <header className="header">
-        header
-      </header>
+      <header className="header">header</header>
       <aside className="sidebar" id="nav">
-      <a href="nav" className="botonMenu">
-          <span class="material-symbols-outlined menu">menu</span>
-        </a>
-        <a href="nav" className="botonClose">
-          <span class="material-symbols-outlined close">close</span>
-        </a>
-        <ul className="sideElem">
-          <li className="sideItem">
-            Perfil
-            <a href="#" className="sideLink"></a>
-          </li>
-          <li className="sideItem">
-            Inventario
-            <a href="#" className="sideLink"></a>
-          </li>
-          <li className="sideItem">
-            Estadisticas
-            <a href="#" className="sideLink"></a>
-          </li>
-          <li className="sideItem">
-            Logros
-            <a href="#" className="sideLink"></a>
-          </li>
-        </ul>
+        <div className="headernav">
+          <div className="logo">
+            <img src={image1} alt="logo1" />
+            <div className="close">
+              <a href="nav" className="botonClose">
+                <span class="material-symbols-outlined close">close</span>
+              </a>
+            </div>
+          </div>
+          <div className="titulo">Nombre empresa</div>
+        </div>
+
+        <div className="element">
+          <ul className="sideElem">
+            <div className="box">
+              <div className="icon"><i class="fa-regular fa-user"></i></div>
+              <li className="sideItem">
+                Perfil
+                <a href="#" className="sideLink"></a>
+              </li>
+            </div>
+            <div className="box">
+              <div className="icon"><i class="fa-solid fa-box-open"></i></div>
+              <li className="sideItem">
+                Inventario
+                <a href="#" className="sideLink"></a>
+              </li>
+            </div>
+            <div className="box">
+              <div className="icon"><i class="fa-sharp fa-solid fa-chart-simple"></i></div>
+              <li className="sideItem">
+                Estadisticas
+                <a href="#" className="sideLink"></a>
+              </li>
+            </div>
+            <div className="box">
+              <div className="icon"><i class="fa-solid fa-trophy"></i></div>
+              <li className="sideItem">
+                Logros
+                <a href="#" className="sideLink"></a>
+              </li>
+            </div>
+            
+          </ul>
+        </div>
       </aside>
       <article className="main">main</article>
       <footer className="footer">footer</footer>
